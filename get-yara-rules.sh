d5ad1b79070e2cace0a5ca5b1007ca30e362284d
jbrown <7155869+dfir-scripts@users.noreply.github.com>
2022-10-27 21:29:54 +0400

    Add files via upload

--- get-yara-rules.sh
+++ get-yara-rules.sh
@@ -3,18 +3,16 @@
 echo "Download/update latest yara rules from Github"
 read -s -n 1 -p " Press a key to continue . . ."
 echo ""
-[ "$(ls -A /usr/local/src/yara/Neo23x0/signature-base/)" ] && \
+[ "$(ls -A /usr/local/src/yara/Neo23x0/signature-base/ 2>/dev/null)" ] && \
 git -C /usr/local/src/yara/Neo23x0/signature-base pull --no-rebase|| \
 git clone https://github.com/Neo23x0/signature-base.git /usr/local/src/yara/Neo23x0/signature-base
-[ "$(ls -A /usr/local/src/yara/reversinglabs/)" ] && \
+[ "$(ls -A /usr/local/src/yara/reversinglabs/ 2>/dev/null)" ] && \
 git -C /usr/local/src/yara/reversinglabs pull --no-rebase || \
 git clone https://github.com/reversinglabs/reversinglabs-yara-rules.git /usr/local/src/yara/reversinglabs
-[ "$(ls -A /usr/local/src/yara/yararules.com/)" ] && \
+[ "$(ls -A /usr/local/src/yara/yararules.com/ 2>/dev/null)" ] && \
 git -C /usr/local/src/yara/yararules.com pull --no-rebase || \
 git clone https://github.com/Yara-Rules/rules.git /usr/local/src/yara/yararules.com
-[ "$(ls -A /usr/local/src/yara/stvemillertime)" ] && \
-git -C /usr/local/src/yara/stvemillertime pull  --no-rebase || \
-git clone https://github.com/stvemillertime/ConventionEngine.git /usr/local/src/yara/stvemillertime
+[ "$(ls -A /usr/local/src/yara/bartblaze/ 2>/dev/null)" ] && \
 git -C /usr/local/src/yara/bartblaze pull  --no-rebase || \
-git clone https://github.com/bartblaze/Yara-rules.git  /usr/local/src/yara/bartblaze
+git clone  https://github.com/bartblaze/Yara-rules.git  /usr/local/src/yara/bartblaze
 echo "Yara rules saved to /urs/local/src/yara/"
